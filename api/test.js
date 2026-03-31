23d0d44b3eeb8a927f85bb930159212149ed4c5e
Pranshu Bhatnagar <pranshubhatnagar.pb@gmail.com>
2026-03-31 13:45:04 +0530

    Add debugging to test function

--- api/test.js
+++ api/test.js
@@ -1,3 +1,8 @@
 module.exports = function handler(req, res) {
-  res.status(200).json({ message: 'API function is working!' });
+  console.log('Function called with method:', req.method);
+  res.status(200).json({ 
+    message: 'API function is working!',
+    method: req.method,
+    timestamp: new Date().toISOString()
+  });
 };
