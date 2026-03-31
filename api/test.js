4aa292f0163c1349a533bc6cdf4a2e408525a390
Pranshu Bhatnagar <pranshubhatnagar.pb@gmail.com>
2026-03-31 13:46:35 +0530

    Switch to ES module format for API functions

--- api/test.js
+++ api/test.js
@@ -1,8 +1,8 @@
-exports.handler = function (req, res) {
+export default function handler(req, res) {
   console.log('Function called with method:', req.method);
   res.status(200).json({ 
     message: 'API function is working!',
     method: req.method,
     timestamp: new Date().toISOString()
   });
-};
+}
